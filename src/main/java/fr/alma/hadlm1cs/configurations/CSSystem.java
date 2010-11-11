56b28fa451722ca332b99dae19f6df37a7841ca6
Alexandre Garnier <alexandre.garnier.86@gmail.com>
2010-11-11 20:20:53 +0100

    CSSystem added

---
+++ src/main/java/fr/alma/hadlm1cs/configurations/CSSystem.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.alma.hadlm1cs.configurations;
+
+import fr.alma.hadl.annotations.Component;
+import fr.univnantes.alma.hadlm2.composant.Configuration;
+
+/**
+ *
+ * @author indy
+ */
+@Component("CSSystem")
+public class CSSystem extends Configuration {
+}
