e85f6ce5b5c3854e3622b71354f10ca10d4f93c6
Alexandre Garnier <alexandre.garnier.86@gmail.com>
2010-11-11 18:36:56 +0100

    First commit

---
+++ src/main/java/com/mycompany/hadlm1cs/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.hadlm1cs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
